aebfb7d9172bbead9940c7774547277ace0924f0
xiphiasnonus <foster.pianist@gmail.com>
2020-03-30 10:56:57 -0400

    Initial Commit

---
+++ Azki/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Azki
+//
+//  Created by Thomas Foster on 3/30/20.
+//  Copyright © 2020 Thomas Foster. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
